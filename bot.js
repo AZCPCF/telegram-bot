82270f20c71c7c3a6ffb021c3ae5641eb2d28203
AZCPCF <99.AZCPCF.99@gmail.com>
2023-12-23 23:13:16 +0330

    add AZCPCF v.1

--- bot.js
+++ bot.js
@@ -1,38 +1,9 @@
-// const { Telegraf } = require('telegraf')
-// const { calculator } = require('./calculator')
-// const bot = new Telegraf('6487623750:AAHMzeRNPcbEvxmrE19KjrrrNMNEc_pFqqA');
-
-// // Command to initiate translation
-const commands = [
-    {
-        command: 'start',
-        function: (ctx) => {
-            ctx.reply('welcome to my AZCPCF test bot')
-        }
-    },
-    {
-        command: 'calculator',
-        function: calculator()
-    }
-]
-// for (const command of commands) {
-//     bot.command(command.command, command.function)
-// }
-// // bot.command('start',(ctx) => {ctx.reply('hello world')})
-// // console.log('first')
-// bot.launch()
-
-// // Handle text messages (math expressions)
 const { Telegraf } = require('telegraf');
-const { handleStartCommand, handleRestartCommand, handleTextMessage } = require('./calculator');
-
+const { calculator } = require('./functions');
 const bot = new Telegraf('6487623750:AAHMzeRNPcbEvxmrE19KjrrrNMNEc_pFqqA');
-
-// Command to start the calculator
-bot.command('start', handleStartCommand);
-
-// Handle text messages (math expressions)
-bot.on('text', handleTextMessage);
-
-// Start the bot
+bot.command('start', (ctx) => {
+    ctx.reply('yo nigga ! welcome to calculator ;)')
+})
+bot.on('text', calculator);
 bot.launch();
+const a = 'سبمل'
---
+++ functions.js
@@ -0,0 +1,26 @@
+function persianToEnglish(persianNumber) {
+    const persianDigits = '۰۱۲۳۴۵۶۷۸۹';
+    const englishDigits = '0123456789';
+
+    const convertedNumber = persianNumber
+        .split('')
+        .map((char) => {
+            const index = persianDigits.indexOf(char);
+            return index !== -1 ? englishDigits[index] : char;
+        })
+        .join('');
+
+    return convertedNumber;
+}
+function calculator(ctx) {
+    const expression = persianToEnglish(ctx.message.text);
+    try {
+        const result = eval(expression);
+        ctx.reply(`result :\n${expression} =  ${result}`);
+    } catch (error) {
+        ctx.reply('Invalid expression. Please enter a valid mathematical expression.');
+    }
+}
+module.exports = {
+    calculator
+}
