3f7b175c9d8e5a72cee009a0f09de4fe09e610bd
AZCPCF <144158983+AZCPCF@users.noreply.github.com>
2023-12-23 23:23:39 +0330

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,24 +1,24 @@
 function persianToEnglish(persianNumber) {
-    const persianDigits = '۰۱۲۳۴۵۶۷۸۹';
-    const englishDigits = '0123456789';
+    const persianDigits = '۰۱۲۳۴۵۶۷۸۹'
+    const englishDigits = '0123456789'
 
     const convertedNumber = persianNumber
         .split('')
         .map((char) => {
-            const index = persianDigits.indexOf(char);
-            return index !== -1 ? englishDigits[index] : char;
+            const index = persianDigits.indexOf(char)
+            return index !== -1 ? englishDigits[index] : char
         })
-        .join('');
+        .join('')
 
-    return convertedNumber;
+    return convertedNumber
 }
 function calculator(ctx) {
-    const expression = persianToEnglish(ctx.message.text);
+    const expression = persianToEnglish(ctx.message.text)
     try {
-        const result = eval(expression);
-        ctx.reply(`result :\n${expression} =  ${result}`);
+        const result = eval(expression)
+        ctx.reply(`result :\n${expression} =  ${result}`)
     } catch (error) {
-        ctx.reply('Invalid expression. Please enter a valid mathematical expression.');
+        ctx.reply('Invalid expression. Please enter a valid mathematical expression.')
     }
 }
 module.exports = {
