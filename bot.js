76fa125ae8db76385376e83c6f34e5afcd14f7be
AZCPCF <144158983+AZCPCF@users.noreply.github.com>
2023-12-23 23:23:11 +0330

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,8 @@
-const { Telegraf } = require('telegraf');
-const { calculator } = require('./functions');
-const bot = new Telegraf('6487623750:AAHMzeRNPcbEvxmrE19KjrrrNMNEc_pFqqA');
+const { Telegraf } = require('telegraf')
+const { calculator } = require('./functions')
+const bot = new Telegraf('6487623750:AAHMzeRNPcbEvxmrE19KjrrrNMNEc_pFqqA')
 bot.command('start', (ctx) => {
     ctx.reply('yo nigga ! welcome to calculator ;)')
 })
-bot.on('text', calculator);
-bot.launch();
-const a = 'سبمل'
+bot.on('text', calculator)
+bot.launch()
